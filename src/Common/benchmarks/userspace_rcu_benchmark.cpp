32821735a8aa09bf07c4d5f18fbdcc8d8ffff147
Azat Khuzhin <a3at.mail@gmail.com>
2020-11-23 21:48:11 +0300

    urcu/benchmarks: measure rcu_read_lock()/rcu_read_unlock()

--- src/Common/benchmarks/userspace_rcu_benchmark.cpp
+++ src/Common/benchmarks/userspace_rcu_benchmark.cpp
@@ -1,5 +1,26 @@
 #include <benchmark/benchmark.h>
 #include <urcu/uatomic.h>
+#if defined(RCU_MEMB)
+#include <urcu.h>
+#elif defined(RCU_BP)
+#include <urcu-bp.h>
+#else
+#error None of RCU_* defined
+#endif
+
+static void BM_userspace_rcu(benchmark::State& state)
+{
+    rcu_init();
+
+    unsigned atomic = 0;
+    while (state.KeepRunning())
+    {
+        rcu_read_lock();
+        ++atomic;
+        rcu_read_unlock();
+    }
+}
+BENCHMARK(BM_userspace_rcu);
 
 static void BM_userspace_rcu_uatomic_add(benchmark::State& state)
 {
