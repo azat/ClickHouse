700b83b0c5d670f2ad326eaabb1e1556abd4dc30
Azat Khuzhin <a3at.mail@gmail.com>
2023-04-30 14:45:28 +0200

    Use packed structure for HASHED layout
    Signed-off-by: Azat Khuzhin <a.khuzhin@semrush.com>

--- src/Dictionaries/HashedDictionary.h
+++ src/Dictionaries/HashedDictionary.h
@@ -25,6 +25,213 @@
 namespace DB
 {
 
+/// A pair that does not initialize the elements, if not needed.
+template <typename First, typename Second>
+struct PackedPairNoInit
+{
+    First first;
+    Second second;
+
+    PackedPairNoInit() {} /// NOLINT
+
+    template <typename FirstValue>
+    PackedPairNoInit(FirstValue && first_, NoInitTag)
+        : first(std::forward<FirstValue>(first_))
+    {
+    }
+
+    template <typename FirstValue, typename SecondValue>
+    PackedPairNoInit(FirstValue && first_, SecondValue && second_)
+        : first(std::forward<FirstValue>(first_))
+        , second(std::forward<SecondValue>(second_))
+    {
+    }
+} __attribute__((packed));
+template <typename First, typename Second>
+PackedPairNoInit<std::decay_t<First>, std::decay_t<Second>> makePairNoInit(First && first, Second && second)
+{
+    return PackedPairNoInit<std::decay_t<First>, std::decay_t<Second>>(std::forward<First>(first), std::forward<Second>(second));
+}
+
+template <typename Key, typename TMapped, typename Hash, typename TState = HashTableNoState>
+struct PackedHashMapCell
+{
+    using Mapped = TMapped;
+    using State = TState;
+
+    using value_type = PackedPairNoInit<Key, Mapped>;
+    using mapped_type = Mapped;
+    using key_type = Key;
+
+    value_type value;
+
+    PackedHashMapCell() = default;
+    PackedHashMapCell(const Key & key_, const State &) : value(key_, NoInitTag()) {}
+    PackedHashMapCell(const value_type & value_, const State &) : value(value_) {}
+
+    /// Get the key (externally).
+    const Key & getKey() const { return value.first; }
+    Mapped & getMapped() { return value.second; }
+    const Mapped & getMapped() const { return value.second; }
+    const value_type & getValue() const { return value; }
+
+    /// Get the key (internally).
+    static const Key & getKey(const value_type & value) { return value.first; }
+
+    bool keyEquals(const Key & key_) const { return bitEquals(value.first, key_); }
+    bool keyEquals(const Key & key_, size_t /*hash_*/) const { return bitEquals(value.first, key_); }
+    bool keyEquals(const Key & key_, size_t /*hash_*/, const State & /*state*/) const { return bitEquals(value.first, key_); }
+
+    void setHash(size_t /*hash_value*/) {}
+    size_t getHash(const Hash & hash) const { return hash(value.first); }
+
+    bool isZero(const State & state) const { return isZero(value.first, state); }
+    static bool isZero(const Key & key, const State & /*state*/) { return ZeroTraits::check(key); }
+
+    /// Set the key value to zero.
+    void setZero() { ZeroTraits::set(value.first); }
+
+    /// Do I need to store the zero key separately (that is, can a zero key be inserted into the hash table).
+    static constexpr bool need_zero_value_storage = true;
+
+    void setMapped(const value_type & value_) { value.second = value_.second; }
+
+    /// Serialization, in binary and text form.
+    void write(DB::WriteBuffer & wb) const
+    {
+        DB::writeBinary(value.first, wb);
+        DB::writeBinary(value.second, wb);
+    }
+
+    void writeText(DB::WriteBuffer & wb) const
+    {
+        DB::writeDoubleQuoted(value.first, wb);
+        DB::writeChar(',', wb);
+        DB::writeDoubleQuoted(value.second, wb);
+    }
+
+    /// Deserialization, in binary and text form.
+    void read(DB::ReadBuffer & rb)
+    {
+        DB::readBinary(value.first, rb);
+        DB::readBinary(value.second, rb);
+    }
+
+    void readText(DB::ReadBuffer & rb)
+    {
+        DB::readDoubleQuoted(value.first, rb);
+        DB::assertChar(',', rb);
+        DB::readDoubleQuoted(value.second, rb);
+    }
+
+    static bool constexpr need_to_notify_cell_during_move = false;
+
+    static void move(PackedHashMapCell * /* old_location */, PackedHashMapCell * /* new_location */) {}
+
+    template <size_t I>
+    auto & get() & {
+        if constexpr (I == 0) return value.first;
+        else if constexpr (I == 1) return value.second;
+    }
+
+    template <size_t I>
+    auto const & get() const & {
+        if constexpr (I == 0) return value.first;
+        else if constexpr (I == 1) return value.second;
+    }
+
+    template <size_t I>
+    auto && get() && {
+        if constexpr (I == 0) return std::move(value.first);
+        else if constexpr (I == 1) return std::move(value.second);
+    }
+
+};
+
+}
+
+namespace std
+{
+    template <typename Key, typename TMapped, typename Hash, typename TState>
+    struct tuple_size<DB::PackedHashMapCell<Key, TMapped, Hash, TState>> : std::integral_constant<size_t, 2> { };
+
+    template <typename Key, typename TMapped, typename Hash, typename TState>
+    struct tuple_element<0, DB::PackedHashMapCell<Key, TMapped, Hash, TState>> { using type = Key; };
+
+    template <typename Key, typename TMapped, typename Hash, typename TState>
+    struct tuple_element<1, DB::PackedHashMapCell<Key, TMapped, Hash, TState>> { using type = TMapped; };
+}
+
+namespace DB
+{
+
+template <size_t initial_size_degree = 8>
+class alignas(64) HashedHashTableGrower
+{
+    /// The state of this structure is enough to get the buffer size of the hash table.
+
+    UInt8 size_degree = initial_size_degree;
+    size_t precalculated_mask = (1ULL << initial_size_degree) - 1;
+    size_t precalculated_max_fill = 1ULL << (initial_size_degree - 1);
+
+public:
+    UInt8 sizeDegree() const { return size_degree; }
+
+    void increaseSizeDegree(UInt8 delta)
+    {
+        size_degree += delta;
+        precalculated_mask = (1ULL << size_degree) - 1;
+        precalculated_max_fill = static_cast<size_t>((1ULL << size_degree) * 0.94);
+
+        fmt::print(stderr, "grower: {}, size_degree: {}, max_fill: {}\n", reinterpret_cast<void*>(this), static_cast<size_t>(size_degree), precalculated_max_fill);
+    }
+
+    static constexpr auto initial_count = 1ULL << initial_size_degree;
+
+    /// If collision resolution chains are contiguous, we can implement erase operation by moving the elements.
+    static constexpr auto performs_linear_probing_with_single_step = true;
+
+    /// The size of the hash table in the cells.
+    size_t bufSize() const { return 1ULL << size_degree; }
+
+    /// From the hash value, get the cell number in the hash table.
+    size_t place(size_t x) const { return x & precalculated_mask; }
+
+    /// The next cell in the collision resolution chain.
+    size_t next(size_t pos) const { return (pos + 1) & precalculated_mask; }
+
+    /// Whether the hash table is sufficiently full. You need to increase the size of the hash table, or remove something unnecessary from it.
+    bool overflow(size_t elems) const { return elems > precalculated_max_fill; }
+
+    /// Increase the size of the hash table.
+    void increaseSize() { increaseSizeDegree(size_degree >= 23 ? 1 : 2); }
+
+    /// Set the buffer size by the number of elements in the hash table. Used when deserializing a hash table.
+    void set(size_t num_elems)
+    {
+        size_degree = num_elems <= 1
+             ? initial_size_degree
+             : ((initial_size_degree > static_cast<size_t>(log2(num_elems - 1)) + 2)
+                 ? initial_size_degree
+                 : (static_cast<size_t>(log2(num_elems - 1)) + 2));
+        increaseSizeDegree(0);
+    }
+
+    void setBufSize(size_t buf_size_)
+    {
+        size_degree = static_cast<size_t>(log2(buf_size_ - 1) + 1);
+        increaseSizeDegree(0);
+    }
+};
+
+template <
+    typename Key,
+    typename Mapped,
+    typename Hash = DefaultHash<Key>,
+    typename Grower = HashedHashTableGrower<>,
+    typename Allocator = HashTableAllocator>
+using PackedHashMap = HashMapTable<Key, PackedHashMapCell<Key, Mapped, Hash>, Hash, Grower, Allocator>;
+
 struct HashedDictionaryConfiguration
 {
     const UInt64 shards;
@@ -34,6 +241,7 @@ struct HashedDictionaryConfiguration
 };
 
 static Int64 hashed_dictionary_allocated = 0;
+/// NOTE: custom allocator does not have realloc, hence sparsehash cannot use memmove (memcpy())
 template <class T>
 class HashedDictionaryAllocator
 {
@@ -195,7 +403,7 @@ private:
     template <typename Value>
     using CollectionTypeNonSparse = std::conditional_t<
         dictionary_key_type == DictionaryKeyType::Simple,
-        HashMap<UInt64, Value, DefaultHash<UInt64>>,
+        PackedHashMap<UInt64, Value, DefaultHash<UInt64>, HashedHashTableGrower<>>,
         HashMapWithSavedHash<StringRef, Value, DefaultHash<StringRef>>>;
 
     using NoAttributesCollectionTypeNonSparse = std::conditional_t<
@@ -217,8 +425,8 @@ private:
     template <typename Value>
     using CollectionTypeSparse = std::conditional_t<
         dictionary_key_type == DictionaryKeyType::Simple,
-        google::sparse_hash_map<UInt64, Value, DefaultHash<KeyType>, std::equal_to<UInt64>, HashedDictionaryAllocator<std::pair<const UInt64, Value>>>,
-        google::sparse_hash_map<StringRef, Value, DefaultHash<KeyType>, std::equal_to<StringRef>, HashedDictionaryAllocator<std::pair<const StringRef, Value>>>>;
+        google::sparse_hash_map<UInt64, Value, DefaultHash<KeyType>, std::equal_to<UInt64>, HashedDictionaryAllocator<google::packed_pair<const UInt64, Value>>>,
+        google::sparse_hash_map<StringRef, Value, DefaultHash<KeyType>, std::equal_to<StringRef>, HashedDictionaryAllocator<google::packed_pair<const StringRef, Value>>>>;
 
     using NoAttributesCollectionTypeSparse = google::sparse_hash_set<KeyType, DefaultHash<KeyType>, std::equal_to<KeyType>, HashedDictionaryAllocator<KeyType>>;
 
