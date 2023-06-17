3681bfe97610bc98f9dc209728e54deaba035ea5
Azat Khuzhin <a3at.mail@gmail.com>
2023-06-17 14:48:17 +0200

    tests

--- tests/queries/0_stateless/01540_partition_pruning_module_moduleLegacy.sql
+++ tests/queries/0_stateless/01540_partition_pruning_module_moduleLegacy.sql
@@ -4,6 +4,8 @@ create table data (key String) engine=MergeTree() order by tuple() partition by 
 insert into data select 'foo' || number::String from numbers(100);
 select _partition_id, min(key), max(key), count() from data group by _partition_id order by _partition_id::UInt64;
 
+-- explain indexes=1 select * from data where cityHash64(key)%20 in (cityHash64('foo60')%20)
+-- explain indexes=1 select * from data where cityHash64(key)%20 in (10)
 select * from (explain indexes=1 select * from data where key = 'foo60') where explain like '%Parts%' or explain like '%Partition%' or explain like '%MinMax%' format LineAsString;
 select * from (explain indexes=1 select * from data where key in ('foo60')) where explain like '%Parts%' or explain like '%Partition%' or explain like '%MinMax%' format LineAsString;
 select * from (explain indexes=1 select * from data where key in ('foo60', 'foo20')) where explain like '%Parts%' or explain like '%Partition%' or explain like '%MinMax%' format LineAsString;
