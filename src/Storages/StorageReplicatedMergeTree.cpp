3c6a2c87b1720e01ad24a59491b059352841d3fc
Michael Kolupaev <michael.kolupaev@clickhouse.com>
2024-02-16 01:52:29 +0000

    Add AlterConversions::supportsMutationCommandType(), flatten vector<vector<MutationCommand>>

--- src/Storages/StorageReplicatedMergeTree.cpp
+++ src/Storages/StorageReplicatedMergeTree.cpp
@@ -8962,7 +8962,7 @@ bool StorageReplicatedMergeTree::canUseAdaptiveGranularity() const
 }
 
 
-std::vector<MutationCommands> StorageReplicatedMergeTree::getAlterMutationCommandsForPart(const DataPartPtr & part) const
+MutationCommands StorageReplicatedMergeTree::getAlterMutationCommandsForPart(const DataPartPtr & part) const
 {
     return queue.getAlterMutationCommandsForPart(part);
 }
