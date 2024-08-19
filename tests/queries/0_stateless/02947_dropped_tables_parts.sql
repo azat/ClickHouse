86543d54476e41b3451fc6b2a608d49e08733ee1
Azat Khuzhin <a3at.mail@gmail.com>
2024-08-19 08:55:54 +0200

    tests: fix 02947_dropped_tables_parts test (did not check system.dropped_tables_parts)
    Signed-off-by: Azat Khuzhin <a.khuzhin@semrush.com>

--- tests/queries/0_stateless/02947_dropped_tables_parts.sql
+++ tests/queries/0_stateless/02947_dropped_tables_parts.sql
@@ -1,4 +1,3 @@
-
 DROP TABLE IF EXISTS 02947_table_1;
 DROP TABLE IF EXISTS 02947_table_2;
 
@@ -7,8 +6,9 @@ CREATE TABLE 02947_table_2 (id Int32) Engine=MergeTree() ORDER BY id;
 INSERT INTO 02947_table_1 VALUES (1),(2);
 INSERT INTO 02947_table_2 VALUES (3),(4);
 
-SELECT database, table, name FROM system.parts WHERE database = currentDatabase() AND startsWith(table, '02947_table_');
-select * from system.dropped_tables_parts format Null;
+SELECT table, name FROM system.parts WHERE database = currentDatabase() AND startsWith(table, '02947_table_') ORDER BY 1, 2;
 
+SET database_atomic_wait_for_drop_and_detach_synchronously=0;
 DROP TABLE 02947_table_1;
 DROP TABLE 02947_table_2;
+SELECT table, name FROM system.dropped_tables_parts WHERE database = currentDatabase() AND startsWith(table, '02947_table_') ORDER BY 1, 2;
