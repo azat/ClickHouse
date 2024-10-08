75c6b525087f0eeb2edaa194ca1bd7722d2612f3
Azat Khuzhin <a3at.mail@gmail.com>
2024-10-08 13:45:37 +0200

    Adjust error message about intersecting parts (remove ZooKeeper note from common code)
    It could happen for MergeTree as well.
    
    Signed-off-by: Azat Khuzhin <a.khuzhin@semrush.com>

--- src/Storages/MergeTree/MergeTreeData.cpp
+++ src/Storages/MergeTree/MergeTreeData.cpp
@@ -1309,7 +1309,7 @@ void MergeTreeData::PartLoadingTree::add(const MergeTreePartInfo & info, const S
             else if (!prev_info.isDisjoint(info))
             {
                 throw Exception(ErrorCodes::LOGICAL_ERROR,
-                    "Part {} intersects previous part {}. It is a bug or a result of manual intervention in the server or ZooKeeper data",
+                    "Part {} intersects previous part {}. It is a bug or a result of manual intervention",
                     name, prev->second->name);
             }
         }
@@ -1326,7 +1326,7 @@ void MergeTreeData::PartLoadingTree::add(const MergeTreePartInfo & info, const S
             else if (!next_info.isDisjoint(info))
             {
                 throw Exception(ErrorCodes::LOGICAL_ERROR,
-                    "Part {} intersects next part {}.  It is a bug or a result of manual intervention in the server or ZooKeeper data",
+                    "Part {} intersects next part {}. It is a bug or a result of manual intervention",
                     name, it->second->name);
             }
         }
